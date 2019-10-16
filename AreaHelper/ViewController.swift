37a20434f282f586b82dbe373b0e06cae4f36c7b
Liam Davidson <ldavidson@lcs.on.ca>
2019-10-16 19:57:30 -0400

    Initial Commit

---
+++ AreaHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AreaHelper
+//
+//  Created by Davidson, Liam on 2019-10-16.
+//  Copyright © 2019 Davidson, Liam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
